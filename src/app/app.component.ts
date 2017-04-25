7c7ebf81818f89f3014db681291ae335f8c0d8cf
Ayo Ayco <mail@ayoayco.com>
2017-04-25 19:49:31 +0800

    add express, add some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Worldz!';
+  title = 'Cloud-Based Intelligent Total Analysis System';
+  description = 'Using Geospatial Wireless Sensors & Mobile Microscopy';
 }
