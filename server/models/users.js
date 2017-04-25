0145d08e426278a65e9b4cc7a42abf75db5c6c0a
Ayo Ayco <ramon.aycojr@gmail.com>
2017-04-25 22:16:20 +0800

    added some to do

--- server/models/users.js
+++ server/models/users.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+//to do: add other user properties
 var User = new Schema({
     username: String,
     password: String
 });
+
 var User = mongoose.model('User', User);
 module.exports = User;
