9c59c80e25f06ea1771d703ddd1abfdc916743f7
Ayo Ayco <mail@ayoayco.com>
2017-04-26 23:26:05 +0800

    add login service

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 
+
 export class AppLoginComponent {
+
+  constructor(private loginService: LoginService) {
+    this.loginService = loginService;
+  }
+
+  login(): void{
+    console.log(this.loginService.login());
+  }
 }
---
+++ src/app/login.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class LoginService {
+  login(): String {
+    return "You got it!";
+  } // stub
+}
