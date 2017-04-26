5638276df8010a96c2a537433f655cf6a200278f
Ayo Ayco <mail@ayoayco.com>
2017-04-26 11:00:10 +0800

    renamed components, added some styles, added font-awesome

---
+++ src/app/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [`
+    .modal-dialog {
+        width: 350px;
+    }
+  `]
+})
+
+export class AppLoginComponent {
+}
--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './app.nav.component.html',
-  styleUrls: ['./app.nav.component.css']
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 
 export class AppNavComponent {
