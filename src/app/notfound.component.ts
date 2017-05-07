863894fef745527db25b9e06f36ac260a4c181ae
Ayo Ayco <mail@ayoayco.com>
2017-05-07 16:19:19 +0800

    added routing

---
+++ src/app/notfound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-notfound',
+    template: `
+    <h1>Page Not Found</h1>
+    `
+})
+
+export class AppPageNotFoundComponent {
+    
+}
