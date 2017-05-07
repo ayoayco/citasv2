be6f0a66637791680ceae7901d148492cdf4e526
Ayo Ayco <ramon.aycojr@gmail.com>
2017-05-07 17:33:28 +0800

    added router links

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    templateUrl:'./home.component.html'
+    templateUrl:'./home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class AppHomeComponent {
-
+  title = 'Cloud-Based Intelligent Total Analysis System';
+  description = 'Using Geospatial Wireless Sensors & Mobile Microscopy';
 }
--- src/app/notfound.component.ts
+++ src/app/notfound.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-notfound',
-    template: `
-    <h1>Page Not Found</h1>
-    `
+    templateUrl: './notfound.component.html',
+    styleUrls: ['./notfound.component.css']
 })
 
 export class AppPageNotFoundComponent {
