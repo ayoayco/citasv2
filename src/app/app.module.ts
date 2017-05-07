047030ff9bc943717003c02fc4b2032ed2daf113
Ayo Ayco <ramon.aycojr@gmail.com>
2017-05-07 16:19:19 +0800

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppNavComponent } from './nav.component';
 import { AppFooterComponent } from './footer.component';
 import { AppLoginComponent } from './login.component';
 import { AppHeaderComponent } from './header.component';
+import { AppHomeComponent } from './home.component';
+import { AppDashboardComponent } from './dashboard.component';
+import { AppPageNotFoundComponent } from './notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppHomeComponent },
+  { path: 'dashboard', component: AppDashboardComponent },
+  { path: '**', component: AppPageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +25,16 @@ import { AppHeaderComponent } from './header.component';
     AppNavComponent,
     AppFooterComponent,
     AppLoginComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppDashboardComponent,
+    AppHomeComponent,
+    AppPageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
