07460303347225421fe5c6a95c901792a9fb015a
Ayo Ayco <mail@ayoayco.com>
2017-05-23 02:37:01 +0800

    if not logged in, don't allow dashboard access

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppSessionService } from './app.session.service';
+import { Router } from '@angular/router';
 
 @Component ({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+    styleUrls: ['./dashboard.component.css'],
+    providers: [ AppSessionService ]
 })
 
-export class AppDashboardComponent {
-
+export class AppDashboardComponent implements OnInit {
+    constructor(
+        private sessionService: AppSessionService,
+        private router: Router
+    ){}
+    ngOnInit() {
+        let loggedIn: boolean = this.sessionService.isLoggedIn();
+        if(!loggedIn){
+            this.router.navigate(['/']);
+        }
+    }
 }
