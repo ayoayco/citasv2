f6cfff1009b13873c47d3dd9c6f6ca6b9e8c5db8
Ayo Ayco <mail@ayoayco.com>
2017-05-24 16:48:01 +0800

    add contact us feature

--- src/app/contact-us.component.ts
+++ src/app/contact-us.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CitasApiService } from './citas.api.service';
 
 @Component({
     selector: 'app-contact-us',
     templateUrl: './contact-us.component.html',
-    styleUrls: ['./contact-us.component.css']
+    styleUrls: ['./contact-us.component.css'],
+    providers: [
+        CitasApiService
+    ]
 })
 
-export class AppContactUsComponent implements OnInit {
-    ngOnInit() {
-        
+export class AppContactUsComponent{
+    email: string;
+    name: string;
+    message: string;
+
+    constructor(
+        private apiService: CitasApiService
+    ){}
+    sendMessage(){
+        this.apiService.contactUs(this.email, this.name, this.message)
+        .then(
+            res => {
+                let data: any = res;
+                console.log(data);
+            }
+        );
     }
 }
