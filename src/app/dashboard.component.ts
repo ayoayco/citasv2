99f5356a6acb1f91e0688c4e386110ba3d7cc78f
Ayo <ramon.aycojr@gmail.com>
2017-06-04 12:26:44 +0100

    add farm class

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { AppSessionService } from './app.session.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
-import { CitasApiService } from './citas.api.service'
-
+import { CitasApiService } from './citas.api.service';
+import { Farm } from './farm';
 @Component ({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -17,7 +17,17 @@ import { CitasApiService } from './citas.api.service'
 export class AppDashboardComponent {
     
     farms: any[];
-    selectedFarm: string;
+    selectedFarm: Farm = {
+        center: [],
+        farm_id: 0,
+        farm_name: "",
+        farm_size: "",
+        foc_present: false,
+        foc_prevention: false,
+        geocode: "",
+        geometry: [],
+        soil_management: false,
+    };
 
     constructor(
         private sessionService: AppSessionService,
@@ -34,13 +44,25 @@ export class AppDashboardComponent {
 
         let data : any;
 
-        this.apiService.getFarms(this.sessionService.getLoggedInKey())
+        this.apiService.getFarmList(this.sessionService.getLoggedInKey())
         .then(
             res => {
                 data = res;
                 if(data.data){
                     this.farms = data.data;
-                    this.selectedFarm = data.data[0].farm_name;
+                    console.log(this.farms);
+                    this.selectedFarm.farm_name = data.data[0].farm_name;
+                    this.selectedFarm.farm_id = data.data[0].farm_id;
+
+                    this.apiService.getFarm(this.sessionService.getLoggedInKey(), this.selectedFarm.farm_id.toString())
+                    .then(
+                        res => {
+                            data = res;
+                            this.selectedFarm = data.data[0];
+                            console.log(this.selectedFarm);
+                        }
+                    );
+
                 }
                 //console.log(this.farms);
             }
@@ -48,8 +70,22 @@ export class AppDashboardComponent {
     }
 
     public selectFarm(name: string){
-        this.selectedFarm = name;
+        this.selectedFarm.farm_name = name;
+        let selectedArr = $.grep(this.farms, function(e){ return e.farm_name == name });
+        this.selectedFarm.farm_id = selectedArr[0].farm_id;
         console.log("select farm: " + this.selectedFarm);
+        console.log("select farm ID: " + this.selectedFarm.farm_id);
+
+        let data: any;
+
+        this.apiService.getFarm(this.sessionService.getLoggedInKey(), this.selectedFarm.farm_id.toString())
+        .then(
+            res => {
+                data = res;
+                this.selectedFarm = data.data[0];
+                console.log(this.selectedFarm);
+            }
+        );
     }
 
 }
---
+++ src/app/farm.ts
@@ -0,0 +1,14 @@
+// usage:
+// import { Farm } from './farm';
+
+export class Farm {
+    center: number[];
+    farm_id: number;
+    farm_name: string;
+    farm_size: string;
+    foc_present: boolean;
+    foc_prevention: boolean;
+    geocode: string;
+    geometry: any[];
+    soil_management: boolean;
+}
