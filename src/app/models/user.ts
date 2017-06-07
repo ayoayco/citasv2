d07c1513fbfe125c2da14547ca964941a7870763
Ayo <ramon.aycojr@gmail.com>
2017-06-07 14:56:50 +0800

    move models to own directory

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+// usage:
+// import { User } from './user';
+
+export class User {
+  username: string;
+  password: string;
+  hashedpw: string;
+  user_type: number;
+  fullname: string;
+  email: string;
+  mobile_number: string;
+}
