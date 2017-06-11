b861cee97c482fc474ed40470336e75fe1ea43f0
Ayo Ayco <ramon.aycojr@gmail.com>
2017-06-11 19:23:50 +0800

    plant analysis plant info layout + style

--- src/app/plant-analysis.plant-info.component.ts
+++ src/app/plant-analysis.plant-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Plant } from './models/plant';
 
+declare let jssor_1_slider_init: any;
+
 @Component({
     selector: 'plant-info',
     templateUrl: './plant-analysis.plant-info.component.html',
@@ -11,5 +13,6 @@ export class PlantAnalysisPlantInfoComponent {
     @Input() selectedPlant: Plant = new Plant();
 
     constructor(){
+        console.log(this.selectedPlant);
     }
 }
