cee0dfa9e0f564f08aff608cb43025c33a8896af
Ayo Ayco <ramon.aycojr@gmail.com>
2017-06-22 17:21:36 +0800

    change data according to selected Tab

--- src/app/sensor-analysis.sensor-info.component.ts
+++ src/app/sensor-analysis.sensor-info.component.ts
@@ -3,55 +3,108 @@ import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 
 declare var require: any;
 export function highchartsFactory() {
-  return require('highcharts');
+    return require('highcharts');
 }
 
 @Component({
     selector: 'sensor-info',
     templateUrl: './sensor-analysis.sensor-info.component.html',
     styleUrls: ['./sensor-analysis.sensor-info.component.css'],
-    providers: [
-        {
+    providers: [{
         provide: HighchartsStatic,
         useFactory: highchartsFactory
-        },
-    ],
+    }, ],
 })
 
-export class SensorAnalysisSensorInfoComponent implements OnChanges{
+export class SensorAnalysisSensorInfoComponent implements OnChanges {
     @Input() readings: any = undefined;
-    @Input() selectedSensorName: string ="";
-    
+    @Input() selectedSensorName: string = "";
+
     options: Object;
 
     constructor() {
+        var $link = $('#tablist');
+    }
+
+    clickTab(tab) {
+        console.log(tab);
+
+        var chartTitle = '';
+        var yAxisLabel = '';
+
+        switch(tab){
+            case 'soil_temp': chartTitle = 'Soil Temperature';
+                yAxisLabel = 'Temperature (°C)'; break;
+            case 'air_temp': chartTitle = 'Air Temperature';
+                yAxisLabel = 'Temperature (°C)'; break;
+            case 'pH': chartTitle = 'pH Level'; break;
+            case 'conductivity': chartTitle = 'Conductivity';break;
+            case 'light': chartTitle = 'Amount of Light';break;
+            case 'moisture': chartTitle = 'Soil Moisture';break;
+            default: chartTitle = 'Chart'; yAxisLabel = 'Values'; break;
+        }
+
+        if (this.readings) {
+            console.log(this.readings);
+            var air_temp = [];
+            var labels = [];
+
+            for (var i = 0; i < this.readings.length; i++) {
+                air_temp.push(this.readings[i][tab]);
+                labels.push(this.readings[i].timestamp);
+            }
+        }
+
+        this.options = {
+            title: { text: chartTitle },
+            colors: ["#19BD6C"],
+            chart: {
+                width: 740
+            },
+            series: [{
+                name: this.selectedSensorName,
+                data: air_temp,
+            }],
+            xAxis: {
+                gridLineWidth: 1,
+                categories: labels
+            },
+            yAxis: {
+                title: {
+                    text: yAxisLabel
+                }
+            }
+        };
 
     }
 
     ngOnChanges(changes: SimpleChanges) {
         //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
         //Add 'implements OnChanges' to the class.
-        if(changes.readings && changes.readings.firstChange == false ){
-            if(this.readings){
+        if (changes.readings && changes.readings.firstChange == false) {
+            if (this.readings) {
                 console.log(this.readings);
                 var air_temp = [];
                 var labels = [];
 
-                for(var i=0; i<this.readings.length; i++){
-                    air_temp.push(this.readings[i].air_temp);
+                for (var i = 0; i < this.readings.length; i++) {
+                    air_temp.push(this.readings[i].soil_temp);
                     labels.push(this.readings[i].timestamp);
                 }
             }
 
             this.options = {
-                title : { text : 'Air Temperature' },
+                title: { text: 'Soil Temperature' },
+                colors: ["#19BD6C"],
                 chart: {
-                    width: 800
+                    width: 740
                 },
                 series: [{
+                    name: this.selectedSensorName,
                     data: air_temp,
                 }],
                 xAxis: {
+                    gridLineWidth: 1,
                     categories: labels
                 },
                 yAxis: {
