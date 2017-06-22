7530de2acb1d31b881862032c4fa9583b5bb7e72
Ayo Ayco <ramon.aycojr@gmail.com>
2017-06-22 11:01:09 +0800

    add highcharts feature

--- src/app/sensor-analysis.sensor-info.component.ts
+++ src/app/sensor-analysis.sensor-info.component.ts
@@ -1,15 +1,35 @@
 import { Component, Input } from '@angular/core';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+declare var require: any;
+export function highchartsFactory() {
+  return require('highcharts');
+}
 
 @Component({
     selector: 'sensor-info',
     templateUrl: './sensor-analysis.sensor-info.component.html',
-    styleUrls: ['./sensor-analysis.sensor-info.component.css']
+    styleUrls: ['./sensor-analysis.sensor-info.component.css'],
+    providers: [
+        {
+        provide: HighchartsStatic,
+        useFactory: highchartsFactory
+        },
+    ],
 })
 
 export class SensorAnalysisSensorInfoComponent{
     @Input() readings: any = undefined;
     @Input() selectedSensorName: string ="";
     
-    constructor(){
+    options: Object;
+
+    constructor() {
+        this.options = {
+            title : { text : 'simple chart' },
+            series: [{
+                data: [29.9, 71.5, 106.4, 129.2],
+            }]
+        };
     }
 }
