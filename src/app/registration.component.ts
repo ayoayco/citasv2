a9726fd2c9b1dbd502aaa5e0ecc66695fb048262
Ayo Ayco <mail@ayoayco.com>
2017-06-26 19:40:22 +0800

    validate mobile number

--- src/app/registration.component.ts
+++ src/app/registration.component.ts
@@ -56,6 +56,11 @@ export class AppRegistrationComponent {
             this.msg += "<li> Mobile Number empty</li>";
             this.err = true;
         }
+        console.log("#: "+this.user.mobile_number.substring(0,3));
+        if(this.user.mobile_number.substring(0,3) != "639"){
+            this.msg += "<li> Mobile Number should start with '639'</li>";
+            this.err = true;
+        }
         if(this.user.user_type == 0){
             this.msg += "<li> User Type empty</li>";
             this.err = true;
