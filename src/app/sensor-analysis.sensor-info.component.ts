c5aab5897a02a50de3f7ec141b3540d181a3ce9e
Ayo Ayco <ramon.aycojr@gmail.com>
2017-06-28 00:25:43 +0800

    collected sensor data table

--- src/app/sensor-analysis.sensor-info.component.ts
+++ src/app/sensor-analysis.sensor-info.component.ts
@@ -19,6 +19,7 @@ export function highchartsFactory() {
 export class SensorAnalysisSensorInfoComponent implements OnChanges {
     @Input() readings: any = undefined;
     @Input() selectedSensorName: string = "";
+    @Input() type: string;
 
     selectedTab = '';
     options: Object;
