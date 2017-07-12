9a370d87e5f5815c6d635ac37881e28014aa5db8
Ayo Ayco <ramon.aycojr@gmail.com>
2017-07-12 20:45:07 +0800

    download plant images filters

--- src/app/downloads.sensor-data.component.ts
+++ src/app/downloads.sensor-data.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CitasApiService } from './citas.api.service';
 import { AppSessionService } from './app.session.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Title } from '@angular/platform-browser';
 
 import { Farm } from './models/farm';
 import { Site } from './models/site';
@@ -24,13 +22,10 @@ export class DownloadSensorDataComponent{
     @Input() selectedFarm: Farm;
     @Input() sites: Site[];
 
-    selectedSiteID: number;
+    selectedSiteID: string= 'all';
     
     constructor(
-        private activeRoute: ActivatedRoute,
         private sessionService: AppSessionService,
-        private router: Router,
-        private titleService: Title,
         private apiService: CitasApiService
     ){
     }
@@ -43,14 +38,14 @@ export class DownloadSensorDataComponent{
         this.apiService.getSensorsFilterDownloadLink(
             this.sessionService.getLoggedInKey(),
             this.selectedFarm.farm_id.toString(),
-            this.selectedSiteID.toString(),
+            this.selectedSiteID,
             this.from,
             this.to
         ).then(
             res => {
                 data = res;
-                console.log(data);
-                //window.open(data.dl_link, '_blank');
+                //console.log(data);
+                window.open(data.dl_link, '_blank');
             }
         );
     }
