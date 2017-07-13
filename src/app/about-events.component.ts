1188b22442c97697513e321810d512cda5043975
Ayo <mail@ayoayco.com>
2017-07-13 22:18:54 +0800

    add about events page

---
+++ src/app/about-events.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CitasApiService } from './citas.api.service';
+
+@Component({
+    selector: 'app-about-events',
+    templateUrl: './about-events.component.html',
+    styleUrls: ['./about-events.component.css'],
+    providers: [
+        CitasApiService
+    ]
+})
+
+export class AppAboutEventsComponent {
+    events: any[];
+    constructor(private apiService: CitasApiService){
+        let data: any;
+        this.apiService.getEvents()
+        .then(
+            res => {
+                data = res;
+                this.events = data.data;
+                console.log(this.events);
+            }
+        );
+    }
+
+    public isOneDay(event){
+        if(event.date_from == event.date_to){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
