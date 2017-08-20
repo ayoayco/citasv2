47eb01183316053cddaf2e224f37bbd3b7fe31ab
Ayo <mail@ayoayco.com>
2017-08-20 13:22:22 +0800

    initial about team cms

--- src/app/about-team.component.ts
+++ src/app/about-team.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { CitasApiService } from './citas.api.service';
 
 @Component({
     selector: 'app-about-team',
     templateUrl: './about-team.component.html',
-    styleUrls: ['./about-team.component.css']
+    styleUrls: ['./about-team.component.css'],
+    providers: [
+        CitasApiService
+    ]
 })
 
-export class AppAboutTeamComponent {}
+export class AppAboutTeamComponent {
+    teams: any[];
+    constructor(
+        private apiService: CitasApiService
+    ) {
+        let data: any;
+        this.apiService.getTeamList()
+        .subscribe(
+            res => {
+                data = res;
+                data = JSON.parse(data._body);
+                this.teams = data.data;
+                console.log(this.teams);
+            }
+        )
+    }
+}
