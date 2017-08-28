f84251c93703832d15217ef80f346fa36bbd14e6
Ayo <ramon.aycojr@gmail.com>
2017-08-28 16:20:22 +0800

    add why modal

---
+++ src/app/why.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-why',
+    templateUrl: 'why.component.html',
+    styleUrls: ['why.component.css']
+})
+
+export class AppWhyComponent {
+    constructor() {
+        console.log('why page');
+    }
+}
