d90af84562dd5b9c3a8afff9b3839fd81eff6394
Ayo Ayco <ramon.aycojr@gmail.com>
2017-10-18 21:50:14 +0800

    update team interface

--- src/app/update-team.component.ts
+++ src/app/update-team.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { AppSessionService } from './app.session.service';
+import { CitasApiService } from './citas.api.service';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'update-team',
     templateUrl: './update-team.component.html',
-    styleUrls: ['./update-team.component.css']
+    styleUrls: ['./update-team.component.css'],
+    providers: [
+        CitasApiService,
+        AppSessionService
+    ]
 })
 
-export class UpdateTeamComponent {
-    constructor(){
-        console.log('Update component!');
-    }
+export class UpdateTeamComponent implements AfterViewInit{
+    teams: any[];
+
+    constructor(
+        private apiService: CitasApiService, 
+        private router: Router,
+        private titleService: Title,
+        private sessionService: AppSessionService
+    ) {
+        const loggedIn: boolean = this.sessionService.isLoggedIn();
+        if (!loggedIn) {
+            this.router.navigate(['/']);
+        } else {
+            this.titleService.setTitle('Update Team');
+        }
+
+        let data;
+
+        this.apiService.getTeamList()
+        .subscribe(
+            res => {
+                data = res;
+                data = JSON.parse(data._body);
+                this.teams = data.data;
+                console.log(this.teams);
+            }
+        )
+   }
+   ngAfterViewInit() {
+        $('[data-toggle="tooltip"]').tooltip(); 
+   }
 }
