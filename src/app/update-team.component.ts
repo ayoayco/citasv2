cb88b828fbf1abaf19c4a85565e3ff236076cef9
Ayo Ayco <mail@ayoayco.com>
2017-10-18 20:56:50 +0800

    update team initial

---
+++ src/app/update-team.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'update-team',
+    templateUrl: './update-team.component.html',
+    styleUrls: ['./update-team.component.css']
+})
+
+export class UpdateTeamComponent {
+    constructor(){
+        console.log('Update component!');
+    }
+}
